ff0a9575912eddd2c0739aa64a0be9ce0a78731c
unknown <bkingsleygreen@gmail.com>
2022-11-21 17:13:05 +0000

    Second Commit

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">Copyright © Welcome Ally</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ comps/Header.js
@@ -0,0 +1,63 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="mb-5">
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="#">Nightbridge</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="d-flex justify-content-center align-items-center gap-4 navbar-nav mx-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-center" href="#">
+                Gigs &<br className="d-none d-lg-block"/>Dates
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Photos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Videos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
